b6bff68246992970c379ca61710f5f2dccad70f4
Jayeh Casbadillo <erroljustinecasbadillo23@gmail.com>
2025-02-06 01:21:16 +0800

    Frontend Update

--- Frontend/notes-app/src/components/Cards/ProfileInfo.jsx
+++ Frontend/notes-app/src/components/Cards/ProfileInfo.jsx
@@ -4,9 +4,9 @@ import { getInitials } from '../../utils/helper';
 const ProfileInfo = ({ onLogout }) => {
   return (
     <div className="flex items-center gap-3">
-        <div className="w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100">{getInitials("John William")}</div>
+        <div className="w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100">{getInitials("Jayeh Cas")}</div>
         <div>
-            <p className="text-sm font-medium">John William</p>
+            <p className="text-sm font-medium">JayeH Casba</p>
             <button className="text-sm text-slate-700 underline" onClick={onLogout}>
                 Logout
             </button>
